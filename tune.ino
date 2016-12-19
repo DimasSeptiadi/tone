5b1eefea611f79c4074c6431f5027189b9232c50
DimasSeptiadi <dimas.septiadi@gmail.com>
2016-12-19 18:45:25 +0700

    Add files via upload

---
+++ tune.ino
@@ -0,0 +1,80 @@
+int speaker= 9;  // membuat variabel Speaker untuk pin 9 Arduino
+void setup() {
+  // void setup dikosongkan
+}
+void loop() {
+
+  tone(speaker, 262);    delay(400);
+  tone(speaker, 330);    delay(500);
+  tone(speaker, 262);    delay(380);
+  tone(speaker, 330);    delay(500);
+  tone(speaker, 349);    delay(500);
+  tone(speaker, 395);    delay(170);
+  noTone(speaker);       delay(300);
+  tone(speaker, 395);    delay(400);
+  noTone(speaker);       delay(500);
+  tone(speaker, 494);    delay(500);
+  //noTone(speaker);       delay(100);
+  tone(speaker, 523);    delay(500);
+  //noTone(speaker);       delay(100);
+  tone(speaker, 494);    delay(450);
+   //noTone(speaker);       delay(100);
+  tone(speaker, 523);    delay(500);
+  //noTone(speaker);       delay(100);
+  tone(speaker, 494);    delay(500);
+  tone(speaker, 395);    delay(700);
+  noTone(speaker);      delay(1000);
+
+  tone(speaker, 262);    delay(400);
+  tone(speaker, 330);    delay(500);
+  tone(speaker, 262);    delay(380);
+  tone(speaker, 330);    delay(500);
+  tone(speaker, 349);    delay(500);
+  tone(speaker, 395);    delay(170);
+  noTone(speaker);       delay(300);
+  tone(speaker, 395);    delay(400);
+  noTone(speaker);       delay(500);
+  tone(speaker, 494);    delay(500);
+  //noTone(speaker);       delay(100);
+  tone(speaker, 523);    delay(500);
+  //noTone(speaker);       delay(100);
+  tone(speaker, 494);    delay(450);
+   //noTone(speaker);       delay(100);
+  tone(speaker, 523);    delay(500);
+  //noTone(speaker);       delay(100);
+  tone(speaker, 494);    delay(500);
+  tone(speaker, 395);    delay(700);
+  noTone(speaker);      delay(1000);
+
+  tone(speaker, 262);    delay(500);
+ tone(speaker, 330);    delay(700);
+ tone(speaker, 395);    delay(800);
+ tone(speaker, 349);    delay(700);
+ noTone(speaker);       delay(10);
+ tone(speaker, 349);    delay(200);
+ tone(speaker, 395);    delay(400);
+ tone(speaker, 349);    delay(400);
+ tone(speaker, 330);    delay(400);
+ tone(speaker, 262);    delay(400); 
+ tone(speaker, 349);    delay(400);  
+ tone(speaker, 330);    delay(400);  
+ tone(speaker, 262);    delay(1000);
+ noTone(speaker);       delay(700);
+
+ tone(speaker, 262);    delay(500);
+ tone(speaker, 330);    delay(700);
+ tone(speaker, 395);    delay(800);
+ tone(speaker, 349);    delay(700);
+ noTone(speaker);       delay(10);
+ tone(speaker, 349);    delay(200);
+ tone(speaker, 395);    delay(400);
+ tone(speaker, 349);    delay(400);
+ tone(speaker, 330);    delay(400);
+ tone(speaker, 262);    delay(400); 
+ tone(speaker, 349);    delay(400);  
+ tone(speaker, 330);    delay(400);  
+ tone(speaker, 262);    delay(1000);
+ noTone(speaker);       delay(2000);
+
+
+}
